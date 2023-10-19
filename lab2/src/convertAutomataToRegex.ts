42dbd366d3f2c43daf7a4d471730ad82b9230e2a
vicadlsn <cadinspect@gmail.com>
2023-10-19 23:47:16 +0300

    fix automata to regex convertion

--- lab2/src/convertAutomataToRegex.ts
+++ lab2/src/convertAutomataToRegex.ts
@@ -1,4 +1,4 @@
-import {label, Automata} from './automata'
+import { label, Automata } from './automata'
 
 type adjacencyMatrix = string[][];
 
@@ -10,13 +10,13 @@ function buildAdjacencyMatrix(automata: Automata): adjacencyMatrix {
         return l;
     })
 
-    let startState: string[] = [...Array<string>(automata.states+2)].map((_, i) => {
+    let startState: string[] = [...Array<string>(automata.states + 2)].map((_, i) => {
         return (i == automata.init) ? label.epsilon : label.empty;
     })
 
-    automata.final.forEach((i) => m[i][automata.states+1] = label.epsilon);
+    automata.final.forEach((i) => m[i][automata.states + 1] = label.epsilon);
 
-    let finalState: string[] = [...Array<string>(automata.states+2)].map(_ => label.empty);
+    let finalState: string[] = [...Array<string>(automata.states + 2)].map(_ => label.empty);
 
     m.push(startState);
     m.push(finalState);
@@ -30,23 +30,11 @@ export function convertDFAToRegex(automata: Automata) {
     }
 
     let transitions: adjacencyMatrix = buildAdjacencyMatrix(automata);
-    let prevStart: number = automata.init;
-    let startIsFinal: boolean = automata.final.indexOf(automata.init) != -1;
-
-    let res: string = ''
-    if (startIsFinal) {
-        res += '|'
-        let loop = transitions[prevStart][prevStart];
-        if ( loop != label.empty) {
-            res += loop + '|';
-        }
-    }
-
     for (let i = 0; i < automata.states; i++) {
         eliminateState(transitions, i);
     }
 
-    return '^(' + res + transitions[automata.states][automata.states+1] + ')$';
+    return '^(' + transitions[automata.states][automata.states + 1] + ')$';
 }
 
 type transitionFromOrTo = { [label: number]: string }
@@ -79,21 +67,19 @@ function getSuccessors(transitions: adjacencyMatrix, state: number): transitionF
     return tr;
 }
 
-function getTransitionsLabel(transitions: adjacencyMatrix, from: number, to: number): string {
-    return transitions[from][to];
-}
 
 function getEliminationLabel(psLabel: string, pLabel: string, loopLabel: string, sLabel: string): string {
-    pLabel == label.empty ? pLabel = '' : (pLabel == label.epsilon ? pLabel = '' : pLabel = `(${pLabel})`);
-    sLabel == label.empty ? sLabel = '' : (sLabel == label.epsilon ? sLabel = '' : sLabel = `(${sLabel})`);
-    loopLabel == label.empty ? loopLabel = '' : (loopLabel == label.epsilon ? loopLabel = '' : loopLabel = `(${loopLabel})*`);
-    psLabel == label.empty ? psLabel = '' : (psLabel == label.epsilon ? psLabel = '' : psLabel);
+    (pLabel == label.empty || pLabel == label.epsilon) ? pLabel = '' : pLabel = `(${pLabel})`;
+    (sLabel == label.empty || sLabel == label.epsilon) ? sLabel = '' : sLabel = `(${sLabel})`;
+    (loopLabel == label.empty || loopLabel == label.epsilon) ? loopLabel = '' : loopLabel = `(${loopLabel})*`;
+
+    psLabel == label.epsilon ? psLabel = '' : (psLabel != label.empty ? psLabel = `(${psLabel})` : psLabel);
 
     if (pLabel == '' && sLabel == '' && loopLabel == '') {
         return psLabel;
     }
 
-    if (psLabel == '') {
+    if (psLabel == label.empty) {
         return pLabel + loopLabel + sLabel;
     }
 
@@ -103,17 +89,17 @@ function getEliminationLabel(psLabel: string, pLabel: string, loopLabel: string,
 function eliminateState(transitions: adjacencyMatrix, state: number): void {
     let predecessors: transitionFromOrTo = getPredecessors(transitions, state),
         successors: transitionFromOrTo = getSuccessors(transitions, state),
-        loopLabel: string = getTransitionsLabel(transitions, state, state);
+        loopLabel: string = transitions[state][state];
 
     for (let p in predecessors) {
         for (let s in successors) {
             transitions[p][s] = getEliminationLabel
-            (
-                getTransitionsLabel(transitions, Number(p), Number(s)),
-                predecessors[p],
-                loopLabel,
-                successors[s],
-            );
+                (
+                    transitions[Number(p)][Number(s)],
+                    predecessors[p],
+                    loopLabel,
+                    successors[s],
+                );
         }
         transitions[p][state] = label.empty;
     }
